c79e6b8f269d85bfc44f04fcd0f5fae202df9150
DS <ds2forwork@gmail.com>
2025-05-14 07:55:30 +0300

    p

--- WAppV/WAppV/obj/Release/net7.0/WAppV.AssemblyInfo.cs
+++ WAppV/WAppV/obj/Release/net7.0/WAppV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAppV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2767a679862a8f9cb81e40e69d8b867a1aa6d4f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d974a51596bff6e536ac8eb45b41bab1b0e63e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAppV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAppV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
